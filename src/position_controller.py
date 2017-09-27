75c67fe14e700469186bab5196e261080a26aa61
Magnus Aarskog <magnusaar@gmail.com>
2017-09-27 13:39:50 +0200

    Prøver å styre via pyhton, men det er ikke så lett

--- src/position_controller.py
+++ src/position_controller.py
@@ -4,39 +4,39 @@
 # https://github.com/JoshMarino/gazebo_and_ros_control/tree/master/rrrbot_files/src
 
 import rospy
-import mathn
+import math
 
 from std_msgs.msg import Float64
 from math import sin,cos,atan2,sqrt,fabs
 
 #Define a RRBot joint positions publisher for joint controllers.
-def rrbot_joint_positions_publisher():
+def five_dof_robotarm_joint_positions_publisher():
 
 	#Initiate node for controlling joint1 and joint2 positions.
-	rospy.init_node('joint_positions_node', anonymous=True)
+    rospy.init_node('joint_positions_node', anonymous=True)
 
 	#Define publishers for each joint position controller commands.
-	pub1 = rospy.Publisher('/rrbot/joint1_position_controller/command', Float64, queue_size=10)
-	pub2 = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
+    pub1 = rospy.Publisher('/five_dof_robotarm/base_to_turntable_controller/command', Float64, queue_size=10)
+	#pub2 = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
 
-	rate = rospy.Rate(100) #100 Hz
+    rate = rospy.Rate(1) #100 Hz
 
 	#While loop to have joints follow a certain position, while rospy is not shutdown.
-	i = 0
-	while not rospy.is_shutdown():
+    i = 0
+    while not rospy.is_shutdown():
 
 		#Have each joint follow a sine movement of sin(i/100).
-		sine_movement = sin(i/100.)
+        sine_movement = sin(i/100.)
 
 		#Publish the same sine movement to each joint.
-		pub1.publish(sine_movement)
-		pub2.publish(sine_movement)
+        pub1.publish(sine_movement)
+		#pub2.publish(sine_movement)
 
-		i = i+1 #increment i
-
-		rate.sleep() #sleep for rest of rospy.Rate(100)
+        i = i+1 #increment i
+        print i 
+        rate.sleep() #sleep for rest of rospy.Rate(100)
 
 #Main section of code that will continuously run unless rospy receives interuption (ie CTRL+C)
 if __name__ == '__main__':
-	try: rrbot_joint_positions_publisher()
-except rospy.ROSInterruptException: pass
+    try: five_dof_robotarm_joint_positions_publisher()
+    except rospy.ROSInterruptException: pass
