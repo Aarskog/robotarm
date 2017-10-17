836e1483eef34c12a951dc4f237cea50aa063961
Magnus <magnusaar@gmail.com>
2017-10-17 15:54:55 +0200

    Test

---
+++ src/plot_robot_data.py
@@ -0,0 +1,162 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#-----
+
+from scipy.interpolate import CubicSpline
+
+
+def create_spline(qd):
+    step = 0.1
+
+    qd_set     = [[],[],[],[],[]]
+    data_space = [[],[],[],[],[]]
+
+
+    #Transform data
+    qd = qd.transpose()
+
+    i = 0
+    for qdi in qd:
+        x = np.arange(0,qdi.shape[1])
+        y = np.squeeze(np.asarray(qdi))
+
+        cs = CubicSpline(x,y)
+
+        data_space[i] = np.arange(0,qdi.shape[1]-1,step)
+
+        qd_set[i]=cs(data_space[i])
+
+        i = i + 1
+
+
+    #plt.plot(data_space[1],qd_set[1])
+    #plt.show()
+    return qd_set
+
+#-----
+
+def readDataFromfile(filename):
+
+    datafile = open(filename,'r')
+    #Load every line except the last one since it may be incomplete, because
+    # ctrl-c is used and cutting in the middle of writing to file is possible.
+    datas = np.genfromtxt(datafile,skip_footer=1)
+
+    data_from_robot = {'qd':[],'q':[],'q_tilde':[],'u':[],'g':[],'t':[]}
+
+    for i in range(0,datas.shape[0]-5,6):
+        data_from_robot['qd'].append(datas[i])
+        data_from_robot['q'].append(datas[i+1])
+        data_from_robot['q_tilde'].append(datas[i+2])
+        data_from_robot['u'].append(datas[i+3])
+        data_from_robot['g'].append(datas[i+4])
+        data_from_robot['t'].append(datas[i+5])
+
+
+    datafile.close()
+    return data_from_robot
+
+def plotJointAngles(dtf):
+
+    t   = [item[0] for item in dtf['t']]
+
+    q1 = [item[0] for item in dtf['q']]
+    q2 = [item[1] for item in dtf['q']]
+    q3 = [item[2] for item in dtf['q']]
+    q4 = [item[3] for item in dtf['q']]
+    q5 = [item[4] for item in dtf['q']]
+
+    qd1 = [item[0] for item in dtf['qd']]
+    qd2 = [item[1] for item in dtf['qd']]
+    qd3 = [item[2] for item in dtf['qd']]
+    qd4 = [item[3] for item in dtf['qd']]
+    qd5 = [item[4] for item in dtf['qd']]
+
+    #Upper and lower limits of y-axis
+    ylimitHI = 70
+    ylimitLO = 0
+
+    #Choose to plot in rad or deg
+    radians = False
+    degrees = True
+    if radians:
+        plt.figure(1)
+
+        plt.subplot(511)
+        plt.plot(t,q1,'r',t,qd1)
+        plt.title('Measured joint angles and desired joint angles')
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_1 (rad)')
+        plt.ylim(ylimitLO,ylimitUP)
+
+        plt.subplot(512)
+        plt.plot(t,q2,'r',t,qd2)
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_2 (rad)')
+        plt.ylim(ylimitLO,ylimitUP)
+
+        plt.subplot(513)
+        plt.plot(t,q3,'r',t,qd3)
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_3 (rad)')
+        plt.ylim(ylimitLO,ylimitUP)
+
+        plt.subplot(514)
+        plt.plot(t,q4,'r',t,qd4)
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_4 (rad)')
+        plt.ylim(ylimitLO,ylimitUP)
+
+        plt.subplot(515)
+        plt.plot(t,q5,'r',t,qd5)
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_5 (rad)')
+        plt.ylim(ylimitLO,ylimitUP)
+
+        plt.show()
+
+    if degrees:
+        plt.figure(2)
+
+        plt.subplot(511)
+        plt.plot(t,np.degrees(q1),'r',t,np.degrees(qd1))
+        plt.title('Measured joint angles and desired joint angles')
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_1 (deg)')
+        plt.ylim(ylimitLO,ylimitHI)
+
+
+        plt.subplot(512)
+        plt.plot(t,np.degrees(q2),'r',t,np.degrees(qd2))
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_2 (deg)')
+        plt.ylim(ylimitLO,ylimitHI)
+
+        plt.subplot(513)
+        plt.plot(t,np.degrees(q3),'r',t,np.degrees(qd3))
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_3 (deg)')
+        plt.ylim(ylimitLO,ylimitHI)
+
+        plt.subplot(514)
+        plt.plot(t,np.degrees(q4),'r',t,np.degrees(qd4))
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_4 (deg)')
+        plt.ylim(ylimitLO,ylimitHI)
+
+        plt.subplot(515)
+        plt.plot(t,np.degrees(q5),'r',t,np.degrees(qd5))
+        plt.xlabel('time(seconds)')
+        plt.ylabel('q_5 (deg)')
+        plt.ylim(ylimitLO,ylimitHI)
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    filename = '/home/magnaars/catkin_ws/src/five_dof_robotarm/src/datafile.txt'
+    dtf = readDataFromfile(filename)
+
+    plotJointAngles(dtf)
---
+++ src/position_controller.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+
+# Source
+# https://github.com/JoshMarino/gazebo_and_ros_control/tree/master/rrrbot_files/src
+
+import rospy
+import math
+import numpy as np
+
+from scipy.interpolate import CubicSpline
+
+from numpy.linalg import inv
+
+from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState
+
+from math import sin,cos,atan2,sqrt,fabs,pi
+
+debug = False
+
+ # Position
+q = np.matrix([0.0,0.0,0.0,0.0,0.0],dtype=float)
+
+ # velocity
+q_dot = np.matrix([0,0,0,0,0],dtype=float)
+
+#Link Masses
+lm = {'m1':0.126+0.05,'m2':0.01+0.03+0.126,'m3':0.126+0.01,'m4':0.126+0.01,'m5':0.126+0.01*2}
+
+#Link Length
+ll = {'l1':0.0506+0.01,'l2':0.0206+0.0635+0.0506,'l3':0.0206+0.0506,'l4':0.0206+0.0506,'l5':0.0506/2+0.01}
+
+#Gravity
+g = -9.81
+
+#Timestamp
+
+timestamp = float(0)
+
+#Position and velocity from Gazebo
+def callback(data):
+    global timestamp
+
+    q[0,0] = data.position[0]
+    q[0,1] = data.position[2]
+    q[0,2] = data.position[3]
+    q[0,3] = data.position[1]
+    q[0,4] = data.position[4]
+
+    q_dot[0,0] = data.velocity[0]
+    q_dot[0,1] = data.velocity[2]
+    q_dot[0,2] = data.velocity[3]
+    q_dot[0,3] = data.velocity[1]
+    q_dot[0,4] = data.velocity[4]
+
+    timestamp = data.header.stamp.secs + float(float(data.header.stamp.nsecs)/float(10**9))
+
+def create_spline(qd):
+    step = 0.01
+
+    qd_set     = [[],[],[],[],[]]
+    data_space = [[],[],[],[],[]]
+
+
+    #Transform data such that we get the joint postitions in each row
+    qd = qd.transpose()
+
+    i = 0
+    for qdi in qd:
+        x = np.arange(0,qdi.shape[1])
+        y = np.squeeze(np.asarray(qdi))
+
+        cs = CubicSpline(x,y)
+
+        data_space[i] = np.arange(0,qdi.shape[1]-1,step)
+
+        qd_set[i]=cs(data_space[i])
+
+        i = i + 1
+
+
+    #plt.plot(data_space[1],qd_set[1])
+    #plt.show()
+    return qd_set
+
+def gravity():
+    global q,q_dot,g,lm,ll,timestamp
+
+    c234 = sin(q[0,1]+q[0,2]+q[0,3])
+    c23 = sin(q[0,1]+q[0,2])
+    c2 = sin(q[0,1])
+
+
+    l23 = lm['m3']*g*(ll['l2']*c2 + ll['l3']/2*c23)
+    l24 = lm['m4']*g*(ll['l2']*c2 + ll['l3']*c23 + ll['l4']/2*c234)
+    l25 = lm['m5']*g*(ll['l2']*c2 + ll['l3']*c23 + (ll['l4']+ll['l5']/2)*c234)
+
+    l34 = lm['m4']*g*(ll['l3']*c23 + ll['l4']/2*c234)
+    l35 = lm['m5']*g*(ll['l3']*c23 + (ll['l4']+ll['l5']/2)*c234)
+
+    l45 = lm['m5']*g*(ll['l4']+ll['l5']/2)*c234
+
+    g_1 = 0
+    g_2 = lm['m2']*g*ll['l2']/2*c2  + l23 + l24 +l25
+    g_3 = lm['m3']*g*ll['l3']/2*c23 + l34 + l35
+    g_4 = lm['m4']*g*ll['l4']/2*c234 +l45
+    g_5 = 0
+    return np.matrix([[g_1],[g_2],[g_3],[g_4],[g_5]])
+
+#Get the next set point from a set of spline points
+def get_qd(qd_set,q,iterations,qd):
+    #Circle of acceptrance
+    coa = 0.01
+
+    for i in range(0,len(iterations)):
+        if (abs(qd[i]-q[0,i]) < coa) and ((iterations[i]+1)<(len(qd_set[i]))):
+            iterations[i] += 1
+            qd[i] = qd_set[i][iterations[i]]
+    return qd
+
+#Define a RRBot joint positions publisher for joint controllers.
+def five_dof_robotarm_joint_positions_publisher():
+    global q,q_dot,g,lm,ll,timestamp
+	#Initiate node for controlling joint1 and joint2 positions.
+    rospy.init_node('joint_positions_node', anonymous=True)
+
+	#Define publishers for each joint position controller commands.
+    pub1 = rospy.Publisher('/five_dof_robotarm/base_to_turntable_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/five_dof_robotarm/second_joint_controller/command', Float64, queue_size=10)
+    pub3 = rospy.Publisher('/five_dof_robotarm/third_joint_controller/command', Float64, queue_size=10)
+    pub4 = rospy.Publisher('/five_dof_robotarm/fourth_joint_controller/command', Float64, queue_size=10)
+    pub5 = rospy.Publisher('/five_dof_robotarm/fifth_joint_controller/command', Float64, queue_size=10)
+
+    #Get the joint positions and velocities
+    sub = rospy.Subscriber('/five_dof_robotarm/joint_states', JointState, callback)
+
+    update_rate = 1000
+    rate = rospy.Rate(update_rate) #100 Hz
+
+
+    # Kp gain
+    kp1 = 1.5
+    kp2 = 5
+    kp3 = kp2
+    kp4 = kp2
+    kp5 = 1.5
+    Kp = np.matrix([
+    [kp1,0,0,0,0],
+    [0,kp2,0,0,0],
+    [0,0,kp3,0,0],
+    [0,0,0,kp4,0],
+    [0,0,0,0,kp5]],
+    dtype=float)
+
+    #Kd gain
+    kd1 = 1
+    kd2 = 0.9
+    kd3 = kd2
+    kd4 = kd2
+    kd5 = 1
+    Kd = np.matrix([
+    [kd1,0,0,0,0],
+    [0,kd2,0,0,0],
+    [0,0,kd3,0,0],
+    [0,0,0,kd4,0],
+    [0,0,0,0,kd5]],
+    dtype=float)
+
+    # Desired joint positions
+    qd = [1,1,1,1,1]
+
+    q0 = [0,0,0,0,0]
+
+    qd_set = create_spline(np.matrix([q0,qd]))
+    qd = np.matrix(qd)
+    iterations = [0,0,0,0,0]
+
+    qda = [0.0,0.0,0.0,0.0,0.0]
+
+	#While loop to have joints follow a certain position, while rospy is not shutdown.
+    i = float(0)
+    datafile = open('/home/magnaars/catkin_ws/src/five_dof_robotarm/src/datafile.txt','w')
+    while not rospy.is_shutdown():
+        qda = get_qd(qd_set,q,iterations,qda)
+        qd = np.matrix(qda)
+        q_tilde = qd-q
+        gravity_gain = gravity()
+
+        #u = Kp*q_tilde - Kd*q_dot + g(q)
+        u = np.matmul(Kp,np.transpose(q_tilde))-np.matmul(Kd,np.transpose(q_dot)) + gravity_gain
+
+        #Send the wanted torques to Gazebo
+        pub1.publish(u[0,0])
+        pub2.publish(u[1,0])
+        pub3.publish(u[2,0])
+        pub4.publish(u[3,0])
+        pub5.publish(u[4,0])
+
+        #Print every second instead of every iteration
+        if (i%update_rate==0 or i==0) and debug:
+            print "\n\n----------------------------------------------------------------"
+            print "iterations = ",iterations
+            print "qd = \n",np.transpose(qd)
+            print "q = \n",np.transpose(q)
+            print "q_tilde = \n",np.transpose(q_tilde)
+            print "u = \n",u
+            print "g = \n",gravity_gain
+            print "t = \n",timestamp
+
+
+        tstamp = np.matrix([timestamp,0,0,0,0])
+
+        #Save robot data to file. Used for plotting
+        np.savetxt(datafile,qd)
+        np.savetxt(datafile,q)
+        np.savetxt(datafile,q_tilde)
+        np.savetxt(datafile,np.transpose(u))
+        np.savetxt(datafile,np.transpose(gravity_gain))
+        np.savetxt(datafile,tstamp)
+
+
+        i = i+1
+        rate.sleep()
+
+
+
+
+#Main section of code that will continuously run unless rospy receives interuption (ie CTRL+C)
+if __name__ == '__main__':
+
+
+    try: five_dof_robotarm_joint_positions_publisher()
+    except rospy.ROSInterruptException: pass
